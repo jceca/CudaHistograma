24d81af8c3538fbab84d72c7bd927739bfe120e8
basestylo <base.dks@gmail.com>
2013-11-15 14:18:42 +0100

    optimizado hilos y bloques - 12ms

--- histograma2.cu
+++ histograma2.cu
@@ -2,44 +2,55 @@
 #include "image.h"
 #include <time.h>
 #define SIZE 2048*1024
-#define BLOCKS 12
+#define BLOCKS 1000
+#define THREADS 256
 
 __global__ void histo_MultiBlock( unsigned char *buffer,long size,unsigned int *histo ) {
 
-__shared__ unsigned int temp[256];
-int i = threadIdx.x + blockIdx.x * blockDim.x;
-int offset= blockDim.x * gridDim.x;
-int memoffset = blockIdx.x * blockDim.x;
-temp[threadIdx.x] = 0;
-__syncthreads();
+	__shared__ unsigned int temp[256];
+	int i = threadIdx.x + blockIdx.x * THREADS;
+	int offset= THREADS * BLOCKS;
+	int memoffset = blockIdx.x * THREADS;
+	temp[threadIdx.x] = 0;
+	__syncthreads();
 
-while(i<size){
-	
-	atomicAdd( &temp[buffer[i]], 1);
-	i+=offset;
-}
-__syncthreads();
-atomicAdd( &(histo[threadIdx.x+memoffset]), temp[threadIdx.x] );
+	while(i<size){
+		atomicAdd( &temp[buffer[i]], 1);
+		i+=offset;
+	}
+	__syncthreads();
+	atomicAdd( &(histo[threadIdx.x+memoffset]), temp[threadIdx.x] );
 }
 
 int main(void){
 
+unsigned int host_histo[BLOCKS][256];
 unsigned char *dev_buffer;
 unsigned int *dev_histo;
+cudaEvent_t start, stop;
+float elapsedTime;
 
 //pedimos memoria
 cudaMalloc( (void**)&dev_buffer, SIZE );
 cudaMemcpy( dev_buffer, image, SIZE, cudaMemcpyHostToDevice );
-
 cudaMalloc( (void**)&dev_histo,256 * BLOCKS * sizeof(long) );
-cudaMemset( dev_histo, 0, 256*BLOCKS *sizeof(int) );
+cudaMemset( dev_histo, 0, 256*BLOCKS *sizeof(long) );
+//Medicion de tiempo de ejecucion
+cudaEventCreate(&start);
+cudaEventCreate(&stop);
+// Start record
+cudaEventRecord(start, 0);
 //KERNEL EXECUTION
-histo_MultiBlock<<<BLOCKS,256>>>(dev_buffer,SIZE,dev_histo);
+histo_MultiBlock<<<BLOCKS,THREADS>>>(dev_buffer,SIZE,dev_histo);
+// Stop event
+cudaEventRecord(stop, 0);
+cudaEventSynchronize(stop);
+cudaEventElapsedTime(&elapsedTime, start, stop); // that's our time!
 
 //KERNEL HAS FINISHED 
-unsigned int host_histo[BLOCKS][256];
+printf("Kernel ejecutado en %3.2f ms\nCon %d bloques y %d hilos\n",elapsedTime,BLOCKS,THREADS);
 //retorno de valores desde dev a host
-cudaMemcpy( host_histo, dev_histo, 256*BLOCKS*sizeof(int),cudaMemcpyDeviceToHost );
+cudaMemcpy( host_histo, dev_histo, 256*BLOCKS*sizeof(int), cudaMemcpyDeviceToHost );
 /*
 for(long i=0;i<256;i++){
 	for(int j=1;j<BLOCKS;j++){
@@ -69,6 +80,8 @@ for(int i=0;i<256;i++){
 	}
 }
 //liberamos memoria
+cudaEventDestroy(start);
+cudaEventDestroy(stop);
 cudaFree(dev_histo);
 cudaFree(dev_buffer);
 
