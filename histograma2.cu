c961e7fb6ccae978c3365ec9779ffb067c360641
basestylo <base.dks@gmail.com>
2013-11-15 15:05:19 +0100

    makefile generado

--- histograma2.cu
+++ histograma2.cu
@@ -4,13 +4,13 @@
 #define SIZE 2048*1024
 #define BLOCKS 1000
 #define THREADS 256
-
 __global__ void histo_MultiBlock( unsigned char *buffer,long size,unsigned int *histo ) {
 
 	__shared__ unsigned int temp[256];
 	int i = threadIdx.x + blockIdx.x * THREADS;
 	int offset= THREADS * BLOCKS;
 	int memoffset = blockIdx.x * THREADS;
+if(threadIdx.x <256)
 	temp[threadIdx.x] = 0;
 	__syncthreads();
 
@@ -19,6 +19,7 @@ __global__ void histo_MultiBlock( unsigned char *buffer,long size,unsigned int *
 		i+=offset;
 	}
 	__syncthreads();
+if(threadIdx.x <256)
 	atomicAdd( &(histo[threadIdx.x+memoffset]), temp[threadIdx.x] );
 }
 
